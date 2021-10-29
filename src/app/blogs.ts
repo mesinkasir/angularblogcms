f207b74fb48224ddc70a70bdcb2c1450cc8c1dad
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-10-29 13:01:18 +0700

    Update blogs.ts

--- src/app/blogs.ts
+++ src/app/blogs.ts
@@ -1,33 +1,102 @@
 export const blogs = [
   {
+    id: '0',
     title: 'Cool Blogs',
-    image: 'https://bstp.sourceforge.io/cov.jpg',
+    image:
+      'https://1.bp.blogspot.com/-21j2SNvdlLg/YLd5OQ-p3fI/AAAAAAAAOcQ/wDfRqqrhlOc9ykchq4pjcO4P35KwXOp0gCLcBGAsYHQ/s2048/26685797_10211230660023258_8492602650567590556_o.jpg',
     description: 'This is a modern and cool blog with angular.',
-    content: 'Hello blogger this time to update and upgrade your blog platform with angular , angular is powerfull stable and fast for using blogging platform, many developer use angular for many project, with angular you can build a new modern blog, with single page application make simple for your blog, lets get started with angular now, if you need to develope or build with angular blog just contac us.'
+    content:
+      'Hello blogger this time to update and upgrade your blog platform with angular , angular is powerfull stable and fast for using blogging platform, many developer use angular for many project, with angular you can build a new modern blog, with single page application make simple for your blog, lets get started with angular now, if you need to develope or build with angular blog just contact us. ',
   },
   {
+    id: '1',
     title: 'Why Angular',
-    image: 'https://bstp.sourceforge.io/voc.jpg',
-    description: 'Why must angular for develope webiste ??',
-    content: 'With angular make easy to create modern website for company profile, artist , art work , shop , restaurant and many more.. with angular you can build modern and luxury website , angular is a modern web application, the best choice for develope modern website with angular framework, so lets get started with angular now.. just contact us if you need a modern website using angular.'
+    image:
+      'https://1.bp.blogspot.com/-QZrkVkQRNkg/YLd5OR6zeNI/AAAAAAAAOcU/LLPcw6EoOwgrtiDe1eABpb6LU0qo0L91QCLcBGAsYHQ/s2048/27747826_10211422624982262_1056869908644083205_o.jpg',
+    description: 'Why must angular for develope website ??',
+    content:
+      'With angular make easy to create modern website for company profile, artist , art work , shop , restaurant and many more.. with angular you can build modern and luxury website , angular is a modern web application, the best choice for develope modern website with angular framework, so lets get started with angular now.. just contact us if you need a modern website using angular.',
   },
   {
+    id: '2',
     title: 'The Angular Blog',
-    image: 'https://bstp.sourceforge.io/ms.jpg',
-    description: 'Hey..this is a open source.. lets get started and download now',
-    content: 'Angular blog a static bloging modern with angular platform , no need databased for this angular blog, and soon we have open source project with angular and integration with realtime firebase databased for next project, so just connect with us and download we source code project, how to install this angular blog ?? just download it, for first you need third party application like node npm / git and angular, after download git you can run npm install -g @angular/cli. for install angular globaly, after install angular you can fork / clone this angular blog or download it, and extract to your project folder, right click and select git bash here and run command : npm install && ng serve , and open your localhost:4200.'
-  },
-    {
-    title: 'Laravel Angular',
-    image: 'https://bstp.sourceforge.io/jis.jpg',
-    description: 'You must try this modern cms for develope modern website',
-    content: 'This is a LARANG yap... the Laravel and Angular all in one for build and develope your modern website, how this content management blog platform work ?? the concept is.. laravel work for backend for create a post and with laravel you can displaying post on your dynamic website to.. and then you can displaying all post on your angular website with install the front end. with this schema make powerfull for develope and make modern website with popular framework, the artisan and the angular, yes... laravel is a popular and modern php framework and integration with angular the best new modern framework for build apps. download on sourceforge link : https://sourceforge.net/projects/larapos/ or on github https://github.com/mesinkasir/larang , for installation you can following instruction video installation : https://www.youtube.com/watch?v=mJM2b5Ok7rg'
+    image:
+      'https://1.bp.blogspot.com/-CouWGZ6LqZY/YLd5NDo2J0I/AAAAAAAAOcE/XkRUIl6UjrArobrq57Wd9_jGdfzV_KdeQCLcBGAsYHQ/s2048/15025293_10207781747962612_1865020663178979881_o.jpg',
+    description:
+      'Hey..this is a open source.. lets get started and download now',
+    content:
+      'Angular blog a static bloging modern with angular platform , no need databased for this angular blog, and soon we have open source project with angular and integration with realtime firebase databased for next project, so just connect with us and download we source code project, how to install this angular blog ?? just download it, for first you need third party application like node npm / git and angular, after download git you can run npm install -g @angular/cli. for install angular globaly, after install angular you can fork / clone this angular blog or download it, and extract to your project folder, right click and select git bash here and run command : npm install && ng serve , and open your localhost:4200. download now on sourceforge : https://sourceforge.net/projects/angularapp/ or fork /clone it on github : https://github.com/mesinkasir/angularblogcms',
   },
   {
+    id: '3',
+    title: 'Laravel Angular',
+    image:
+      'https://1.bp.blogspot.com/-kIzWZ_rFlQ4/YDnHMYfKo8I/AAAAAAAANHo/Re_wW259f8sBC-YkQPFj_rW8iNqKbU38ACLcBGAsYHQ/s1366/laravel%2Bblog%2Bcms%2Bnew%2B%25281%2529.png',
+    description: 'You must try this modern cms for develope modern website',
+    content:
+      'This is a LARANG yap... the Laravel and Angular all in one for build and develope your modern website, how this content management blog platform work ?? the concept is.. laravel work for backend for create a post and with laravel you can displaying post on your dynamic website to.. and then you can displaying all post on your angular website with install the front end. with this schema make powerfull for develope and make modern website with popular framework, the artisan and the angular, yes... laravel is a popular and modern php framework and integration with angular the best new modern framework for build apps. download on sourceforge link : https://sourceforge.net/projects/larapos/ or on github https://github.com/mesinkasir/larang , for installation you can following instruction video installation : https://www.youtube.com/watch?v=mJM2b5Ok7rg',
+  },
+  {
+    id: '4',
     title: 'Angular POS',
-    image: 'https://bstp.sourceforge.io/si.jpg',
+    image:
+      'https://1.bp.blogspot.com/-HDlZpf8sGE8/YLd5J8MFzdI/AAAAAAAAObk/LB86TAVCbZAzmyTfjOpPmTIcRTWioVwpQCLcBGAsYHQ/s2048/12768318_10205850771729413_4746132550456798237_o.jpg',
     description: 'need point of sale software with angular pos ??',
-    content: 'If you need point of sale with angular for using front end to check all report detail, you can using angular pos m this is a we open source code project with integration to chromis pos and unincenta pos , yess... unicenta and chromis is legend of point of sale, so you can using point of sale with upgrade to online mode , on clouds mode we have develope open source project using angular you can call it angular bakcoffice online for chromispos and uncicenta pos, with front end backoffice make easy to check all report on your bussines and shop, this is free you can download it, but before download angular pos , you must install chromis or unicenta pos, becasue angular pos integration with this software. lets download now : https://sourceforge.net/projects/chromis-unicenta-online-apps/ or fork clone it https://github.com/mesinkasir/chromispos-unicentapos-angular-apps for installation just follow video installation procedure : https://www.youtube.com/watch?v=-Gv1Ibi2OeM&t=5s '
-  }
+    content:
+      'If you need point of sale with angular for using front end to check all report detail, you can using angular pos m this is a we open source code project with integration to chromis pos and unincenta pos , yess... unicenta and chromis is legend of point of sale, so you can using point of sale with upgrade to online mode , on clouds mode we have develope open source project using angular you can call it angular bakcoffice online for chromispos and uncicenta pos, with front end backoffice make easy to check all report on your bussines and shop, this is free you can download it, but before download angular pos , you must install chromis or unicenta pos, becasue angular pos integration with this software. lets download now :  or fork clone it https://github.com/mesinkasir/chromispos-unicentapos-angular-apps for installation just follow video installation procedure : https://www.youtube.com/watch?v=-Gv1Ibi2OeM&t=5s , and if you need a calculator digital with angular framework you can using we a open source code project, just download it on sourceforge : https://sourceforge.net/projects/angularpos/ or fork it from github : https://github.com/mesinkasir/angularpos-kiospos you can see video installation and tutorial : https://www.youtube.com/watch?v=I4zuwV203Kk ',
+  },
+  {
+    id: '5',
+    title: 'Phoneixs Angular',
+    image: 'https://phoneixsangular.web.app/assets/fire.jpg',
+    description: 'New Modern Angular CMS BLogs , Fire Now !!',
+    content:
+      'Here we go.. a new angular cms blog with phoneixs angular, integration with firebase real time database make your angular website dynamic , fast blogs angular using firebase integration , simple using this cms for make and build your new modern website, support with login area admin backend, post page article ,  Open Source code project you can modify with you needed, you can deploy on github pages integration with vercel or hreoku, or with firebase , download and install it , for first must download and install third party apps + git bash + npm + angular , register on firebase and create project - real time database, download phoneixs angular extract and config envirmoent file with your database including API. , run : npm install , ng serve , and open localhost:4200, for detail just open readme.md, This time to upgrade your blog and website using phoneixs angular cms blog. download now : https://sourceforge.net/projects/phoneixs/ or fork clone it https://github.com/mesinkasir/phoneixs for installation just follow video installation procedure : https://www.youtube.com/watch?v=EA5JpxEgApU&t=50s  ',
+  },
+  {
+    id: '6',
+    title: 'Angular Online Shop',
+    image:
+      'https://1.bp.blogspot.com/-dwpDbfNdJ70/YLd5MQU160I/AAAAAAAAOb8/zDCNRp6w7bgsujptygxmV_YCo_J528EjwCLcBGAsYHQ/s2048/14480467_10207391171958456_7419748018999372263_o.jpg',
+    description: "Let's get started build modern online shop",
+    content:
+      'With angular you can develope modern online shop web app , single page application make simple and fast for transaction, the features : home page landing page modern and cool design including product with image and price , on product page details ready image , content and description , pricing about product for inform to your customer, chart , checkout , payment information , and more this is a new modern online shop web app, you can download it free for develope your modern online shop, how to download just play video demo, link donwload in video https://www.youtube.com/watch?v=-xXcpctuheo.',
+  },
 
+  {
+    id: '7',
+    title: 'Build a Mobile Apps',
+    image:
+      'https://1.bp.blogspot.com/-KdOmgew5AAY/YLd5K6bxNvI/AAAAAAAAObs/470DMPEoFTEOiUz8sypDkUiHaDT0qqdcQCLcBGAsYHQ/s2048/14188290_10207230576463669_4891828439591670232_o.jpg',
+    description:
+      'Develope website APK android and IOS apps using angular feat ionic',
+    content:
+      'Angular blog a static bloging modern with angular platform , no need databased for this angular blog, and soon we have open source project with angular and integration with realtime firebase databased for next project, so just connect with us and download we source code project, how to install this angular blog ?? just download it, for first you need third party application like node npm / git and angular, after download git you can run npm install -g @angular/cli. for install angular globaly, after install angular you can fork / clone this angular blog or download it, and extract to your project folder, right click and select git bash here and run command : npm install && ng serve , and open your localhost:4200. download now on on sourceforge : https://sourceforge.net/projects/angularapp/ or fork /clone it on github : https://github.com/mesinkasir/angularblogcms',
+  },
+  {
+    id: '8',
+    title: 'Angular Lovers',
+    image: 'https://angular.io/assets/images/logos/angular/angular.svg',
+    description: 'Website we dev using angular',
+    content:
+      'Many site we dev using angular framework, angular is cool with typescript new generation, make your site blast and fast using angular, list a example site dev using angular : https://phoneixsangular.web.app ,https://smppgrisedati.web.app/ , https://axcoraschool.web.app   ',
+  },
+  {
+    id: '9',
+    title: 'SEO With Angular',
+    image:
+      'https://1.bp.blogspot.com/-lggE-jtzGTk/X9cP5LHussI/AAAAAAAALo0/WpcPYFM60hctEwhp019fd4pG3M5InTuagCLcBGAsYHQ/s800/aplikasi%2Bkasir%2Bonline.gif',
+    description: 'SEO Tips with angular framework',
+    content:
+      "If you create a website project using angular you know if angular is a single page application or progresive web apps, using angular you just make a single title right . This is a diferent if you using gatsby js to build a modern website, with gatsby you can create a title make simple on your pages. Oke.. back to angular, now how to create your website angular powerfull in google SEO, for first with update angular in version 10. you can insert a title on your page. with title on your page make your website super on SEO , now how we do that..?? Of course for first you need insert title code on app.module.ts file.open your text editor then just insert code like this, you just need insert title text in to code : import { BrowserModule, Title } from '@angular/platform-browser';  and providers: [ Title ], then Save it.  After you change the code now you need go to your page component , and edit page.componet.ts and insert this code : import { Title } from '@angular/platform-browser'; And don't forget to change constructor too.. constructor(private title: Title) {} ngOnInit() { this.title.setTitle('Angular is modern web app'); } } Save it.. Now you can run your angular website... test it and visit your page.. and violaaa... your page is now can use the title  ",
+  },
+  {
+    id: '10',
+    title: 'Ng Build Now',
+    image:
+      'https://1.bp.blogspot.com/-gObiWzkVDKM/X6WCMCB8sgI/AAAAAAAALkE/oIpdRr3UZ4sNNHBHd0SWc4r2ioUTBQ3jQCLcBGAsYHQ/s1328/Screenshot_2020-10-13%2BAngular.png',
+    description: 'Develope and deploy your website app for free',
+    content:
+      'If you need to online your website you can using many option : download this source code , register on vercel or heroku host , and resgiter to github bitbucket gitlab for deploy your code, then upload source code project using cli like git push and integration with your vercel heroku and now your site is live, Option two : you can using firebase just register on firebase then install firebase cli with npm install -g firebase-tools using npm, or you can download node npm before run this command,run firebase login with cli for login , firebase init , then develope your project in localhost , for deploy on firebase for first you need to change deploy project, just open on firebase.json file , and configuratoin with your project folder , then open firebase concole create new project web app , name it with your own, and if you website ready you can integrated with firebase and use cli shell run command firebase deploy , or you can read firebase documentation how to deploy from localhost to firebase host,For deploy live and online you must Thanks for stackblitz and firebase, now you can deploy and develope your web app project for free fast and easy, yess... using stackblitz and host for online with firebase, for first of course you need upload code on stackblitz or fork this blog, then register on firebase ,then login to your firebase account open or create new project console, and name it a project with your own, now you need to create a project web app in firebase then name it a project with your site , then copy firebase cdn ,back to stackblitz paste cdn firebase in to <body> tag , check your project code if finish and ready you can deploy with clik on firebase logo, select your firebase console and viola... now your website is online',
+  },
 ];
